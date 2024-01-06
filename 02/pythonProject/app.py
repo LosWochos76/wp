dd8afd34a04b6f8303e74e9e974b5735eccd95cf
Alex <alexander.stuckenholz@gmail.com>
2024-01-06 18:58:19 +0100

    Working on chapter 2

--- 02/pythonProject/app.py
+++ 02/pythonProject/app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template('index.html')
+    return "<h1>Hello World!</h1>"
+@app.route('/username/<username>')
+def username(username):
+    return f"Your username is {username}"
+@app.route("/json")
+def json():
+    data = {'name': 'alex', 'age': 48}
+    return make_response(data, 200, {'Content-Type': 'application/json'})
+@app.route("/umleitung")
+def umleitung():
+    return redirect(url_for("root"))
 
-@app.route("/order", methods=['get', 'post'])
-def order():
-    if request.method == 'GET':
-        return render_template('order.html')
-    else:
-        return render_template('deliver.html')
+@app.route("/template")
+def tempate():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
