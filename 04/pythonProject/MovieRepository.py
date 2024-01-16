077f93d021d2ee4f198d850c6af5d3bfae71401e
Alex <alexander.stuckenholz@gmail.com>
2024-01-16 17:07:13 +0100

    Working on chapter 4

--- 04/pythonProject/MovieRepository.py
+++ 04/pythonProject/MovieRepository.py
@@ -7,16 +7,18 @@ def get_last_page():
     return len(df) // page_size
 
 def get_movie(movie_id):
-    return df.loc[df['ID']==movie_id].to_dict('records')[0]
+    return df.loc[df['ID'] == int(movie_id)].to_dict('records')[0]
 
 def delete(movie_id):
     global df
     df = df[df['ID'] != movie_id]
+    return True
 
 def update(values):
-    #pos = get_pos(int(values['ID']))
-    #for key, value in values.items():
-    #    movies[pos][key] = type(movies[pos][key])(value)
+    global df
+    index = df[df['ID'] == int(values['ID'])].index
+    for key, value in values.items():
+        df.loc[index, key] = pd.Series(value).astype(df[key].dtype).item()
     return True
 
 
@@ -24,6 +26,3 @@ page_size = 10
 df = pd.read_csv('data/movies.csv')
 df = df.sort_values(by=['Film'])
 df['ID'] = range(1, len(df)+1)
-
-movies = df.to_dict(orient='records')
-movies = sorted(movies, key=lambda x: x['Film'])
