25841b0483f87c87611eba3724bd6ebe6875a257
Alexander Stuckenholz <alexander.stuckenholz@gmail.com>
2024-02-12 15:49:54 +0100

    Working

---
+++ 08/pythonTestProject/app.py
@@ -0,0 +1,13 @@
+import math
+def is_prime(number):
+    if number < 2:
+        return False
+
+    if number == 2:
+        return True
+
+    for divisor in range(3, int(number/2), 2):
+        if number % divisor == 0:
+            return False
+
+    return True
