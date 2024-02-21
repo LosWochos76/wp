ffef41953a05ce3da06f89f56d4aa1fe9afe312f
Alexander Stuckenholz <alexander.stuckenholz@2motion.net>
2024-02-21 15:51:13 +0100

    Working

--- 08/test_project/tests/test_app.py
+++ 08/test_project/tests/test_app.py
@@ -1,26 +1,20 @@
 import pytest
 from test_project import app
-from pytest_mock import MockFixture
 
 def test_2():
     assert app.is_prime(2), "2 is a prime!"
 
-
 def test_3():
     assert app.is_prime(3), "3 is a prime!"
-
 
 def test_5():
     assert app.is_prime(5), "5 is a prime!"
 
-
 def test_9():
     assert app.is_prime(9) is False, "9 is not a prime!"
 
-
 def test_97():
     assert app.is_prime(97), "97 is a prime!"
-
 
 def test_100():
     assert app.is_prime(100) is False, "100 is not a prime!"
